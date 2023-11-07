d9e8b506ff28324296f13231be2e7ab0f3904432
StevenWu111 <w1804013863@gmail.com>
2023-11-07 21:39:53 +1100

    GiantBuddha searching function update

--- Source/GameOff/GiantBuddha.cpp
+++ Source/GameOff/GiantBuddha.cpp
@@ -3,24 +3,78 @@
 
 #include "GiantBuddha.h"
 
+#include "GameOffCharacter.h"
+#include "Kismet/KismetMathLibrary.h"
+
 // Sets default values
 AGiantBuddha::AGiantBuddha()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MainMestComponent"));
+	ConeDetectorComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DetectorMesh"));
+	LightComponent = CreateDefaultSubobject<USpotLightComponent>(TEXT("LightComponent"));
+	MeshComponent->SetupAttachment(RootComponent);
+	LightComponent->SetupAttachment(MeshComponent);
+	ConeDetectorComponent->SetupAttachment(MeshComponent);
 }
+
 
 // Called when the game starts or when spawned
 void AGiantBuddha::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	ConeDetectorComponent->OnComponentBeginOverlap.AddDynamic(this,&AGiantBuddha::DetectorOverlapBegin);
+}
+
+void AGiantBuddha::DetectorOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (AGameOffCharacter* Player = Cast<AGameOffCharacter>(OtherActor))
+	{
+		MainPlayer = Player;
+		CurrStatus = Tracking;
+		LightComponent->SetIntensity(LightChangeIntensity);
+	}else if (!TargetActors.IsEmpty() && OtherActor == TargetActors[CurrIndex] && CurrStatus == Searching)
+	{
+		if (CurrIndex == TargetActors.Num()-1)
+		{
+			CurrIndex = 0;
+		}
+		else
+		{
+			CurrIndex++;
+		}
+	}
 }
 
 // Called every frame
 void AGiantBuddha::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	switch (CurrStatus)
+	{
+	case Tracking:
+		break;
+	default:
+		if (!TargetActors.IsEmpty())
+		{
+			RotateToAPoint(TargetActors[CurrIndex], DeltaTime);
+		}
+		break;
+	}
+}
 
+void AGiantBuddha::RotateToAPoint(const AActor* TargetActor, float DeltaTime)
+{
+	if (TargetActor)
+	{
+		const FVector CurrLocation = this->GetActorLocation();
+		const FVector TargetLocation = TargetActor->GetActorLocation();
+		const FRotator RotationBetweenStartNTarget = UKismetMathLibrary::FindLookAtRotation(CurrLocation, TargetLocation);
+		const FRotator NextRotation = UKismetMathLibrary::RInterpTo(this->GetActorRotation(),RotationBetweenStartNTarget, DeltaTime, RotationSpeed);
+		this->SetActorRotation(NextRotation);
+	}
 }
 
--- Source/GameOff/GiantBuddha.h
+++ Source/GameOff/GiantBuddha.h
@@ -3,9 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameOffCharacter.h"
+#include "Components/SpotLightComponent.h"
 #include "GameFramework/Actor.h"
 #include "GiantBuddha.generated.h"
 
+enum GiantBuddhaStatus
+{
+	Searching,
+	Tracking
+};
 UCLASS()
 class GAMEOFF_API AGiantBuddha : public AActor
 {
@@ -15,17 +22,39 @@ public:
 	// Sets default values for this actor's properties
 	AGiantBuddha();
 
-	TArray<AActor> TargetActors;
+	UPROPERTY(EditAnywhere)
+	TArray<AActor*> TargetActors;
 
 	UPROPERTY(EditAnywhere)
 	UStaticMeshComponent* MeshComponent;
 
 	UPROPERTY(EditAnywhere)
 	UStaticMeshComponent* ConeDetectorComponent;
+
+	GiantBuddhaStatus CurrStatus = Searching;
+
+	UPROPERTY(EditAnywhere)
+	USpotLightComponent* LightComponent;
+
+	void RotateToAPoint(const AActor* TargetActor, float DeltaTime);
+
+	UPROPERTY(EditAnywhere)
+	float RotationSpeed = 10.0f;
+
+	UPROPERTY(EditAnywhere)
+	float LightChangeIntensity = 1500;
+
+	int32 CurrIndex = 0;
 	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void DetectorOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	AGameOffCharacter* MainPlayer;
 
 public:	
 	// Called every frame
