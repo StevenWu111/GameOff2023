f425257c22007ba3cece33c8ce7cc3b35c44d6df
StevenWu111 <w1804013863@gmail.com>
2023-11-15 14:08:08 +1100

    interact interface upload

---
+++ Source/GameOff/LaserBuddha.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "GameFramework/Actor.h"
+#include "LaserBuddha.generated.h"
+
+UCLASS()
+class GAMEOFF_API ALaserBuddha : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALaserBuddha();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Interact")
+	void Interact();
+	
+
+};
